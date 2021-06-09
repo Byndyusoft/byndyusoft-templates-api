14a91ac2346e2999eeb3d981ba9d916006f07cf4
Ansar Kanybekov <akanybekov@byndyusoft.com>
2021-06-09 12:46:00 +0500

    Добавил проект мигратора

---
+++ src/Byndyusoft.Template.Migrator/Migrations/TemplateMigration.cs
@@ -0,0 +1,13 @@
+﻿namespace Byndyusoft.Template.Migrator.Migrations
+{
+    using FluentMigrator;
+
+    [Migration(202106091240)]
+    public class TemplateMigration : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
