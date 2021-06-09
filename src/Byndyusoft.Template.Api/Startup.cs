9207a72554a48e09a98945025e0e7f2c995e2ad5
Ansar Kanybekov <akanybekov@byndyusoft.com>
2021-06-09 11:19:30 +0500

    Добавил инфру для swagger'a

--- src/Byndyusoft.Template.Api/Startup.cs
+++ src/Byndyusoft.Template.Api/Startup.cs
@@ -3,8 +3,14 @@ namespace Byndyusoft.Template.Api
     using System.Text.Json.Serialization;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.ApiExplorer;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Options;
+    using Swagger;
+    using Swashbuckle.AspNetCore.SwaggerGen;
+    using Swashbuckle.AspNetCore.SwaggerUI;
     using Tracing;
 
     public class Startup
@@ -13,6 +19,22 @@ namespace Byndyusoft.Template.Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApiVersioning(options =>
+            {
+                options.DefaultApiVersion = ApiVersion.Default;
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.ReportApiVersions = true;
+            });
+
+            services.AddVersionedApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+            });
+
+            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+            services.AddSwaggerGen();
+
             services.AddControllers(options =>
                     {
                         options.PassRequestsToTracer()
@@ -22,10 +44,22 @@ namespace Byndyusoft.Template.Api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApiVersionDescriptionProvider apiVersionDescriptionProvider)
         {
             if (env.IsDevelopment())
-                app.UseDeveloperExceptionPage();
+            {
+                app.UseDeveloperExceptionPage()
+                   .UseSwagger()
+                   .UseSwaggerUI(options =>
+                   {
+                       foreach (var apiVersionDescription in apiVersionDescriptionProvider.ApiVersionDescriptions)
+                           options.SwaggerEndpoint($"/swagger/{apiVersionDescription.GroupName}/swagger.json", apiVersionDescription.GroupName.ToUpperInvariant());
+
+                       options.DisplayRequestDuration();
+                       options.DefaultModelRendering(ModelRendering.Model);
+                       options.DefaultModelExpandDepth(3);
+                   });
+            }
 
             app.UseRouting();
 
