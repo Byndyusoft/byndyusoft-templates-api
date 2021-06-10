4d2e5d04f06c2273b0bdcccb7490c3beda76bf88
Ansar Kanybekov <akanybekov@byndyusoft.com>
2021-06-10 15:19:13 +0500

    Добавил проект с интеграционными тестами на TestHost

---
+++ tests/Byndyusoft.Template.IntegrationTests/Fixtures/ServicesCollectionFixture.cs
@@ -0,0 +1,46 @@
+﻿namespace Byndyusoft.Template.IntegrationTests.Fixtures
+{
+    using System;
+    using System.IO;
+    using Api.Installers;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class ServicesCollectionFixture
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public ServicesCollectionFixture()
+        {
+            var configuration = CreateConfiguration();
+
+            _serviceProvider = CreateServiceProvider(configuration);
+        }
+
+        private IConfiguration CreateConfiguration()
+        {
+            var path = Directory.GetCurrentDirectory();
+
+            var configuration = new ConfigurationBuilder().SetBasePath(path)
+                                                          .AddJsonFile("appsettings.Development.json", false, false)
+                                                          .AddEnvironmentVariables()
+                                                          .Build();
+
+            return configuration;
+        }
+
+        private IServiceProvider CreateServiceProvider(IConfiguration configuration)
+        {
+            var serviceProvider = new ServiceCollection().AddApplicationServices()
+                                                         .AddLogging()
+                                                         .BuildServiceProvider();
+
+            return serviceProvider;
+        }
+
+        public TService GetService<TService>()
+        {
+            return _serviceProvider.GetRequiredService<TService>();
+        }
+    }
+}
