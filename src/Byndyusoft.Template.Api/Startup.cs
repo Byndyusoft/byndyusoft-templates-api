156e038e7120ad82712976db2631aa748d0ad508
rgaynetdinov <foxcoolman@byndyusoft.com>
2021-06-24 16:10:28 +0500

    Добавил проект и доступ к БД

--- src/Byndyusoft.Template.Api/Startup.cs
+++ src/Byndyusoft.Template.Api/Startup.cs
@@ -7,9 +7,11 @@ namespace Byndyusoft.Template.Api
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.ApiExplorer;
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
     using Microsoft.Extensions.Options;
+    using Npgsql;
     using Prometheus;
     using Swagger;
     using Swashbuckle.AspNetCore.SwaggerGen;
@@ -18,10 +20,19 @@ namespace Byndyusoft.Template.Api
 
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRelationalDb(NpgsqlFactory.Instance, Configuration.GetConnectionString("Main"));
+
             services.AddApiVersioning(options =>
             {
                 options.DefaultApiVersion = ApiVersion.Default;
