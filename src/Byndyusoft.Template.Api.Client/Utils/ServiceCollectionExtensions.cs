9287d3407518df4f1b2723898fec4275050c2260
Ansar Kanybekov <akanybekov@byndyusoft.com>
2021-07-07 17:12:50 +0500

    Исправление удаления.

--- src/Byndyusoft.Template.Api.Client/Utils/ServiceCollectionExtensions.cs
+++ src/Byndyusoft.Template.Api.Client/Utils/ServiceCollectionExtensions.cs
@@ -15,7 +15,7 @@
         /// <param name="configuration"></param>
         public static void AddTemplateClient(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddSingleton<ITemplateClient, TemplateClient>();
+            services.AddHttpClient<ITemplateClient, TemplateClient>();
 
             services.Configure<TemplateApiSettings>(configuration.GetSection(nameof(TemplateApiSettings)));
         }
