b8a4dd2b86d08b06445be3878e6c2e34a13807b1
Ansar Kanybekov <akanybekov@byndyusoft.com>
2022-03-24 14:54:27 +0500

    обновили версии пакетов, прибрались

--- templates/src/Api.Client/Clients/TemplateClient.cs
+++ templates/src/Api.Client/Clients/TemplateClient.cs
@@ -3,7 +3,7 @@
     using System;
     using System.Net.Http;
     using System.Threading.Tasks;
-    using Byndyusoft.ApiClient;
+    using ApiClient;
     using Microsoft.Extensions.Options;
     using OpenTracing;
     using OpenTracing.Tag;
