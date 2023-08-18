9713c8ea4a59a64bffb2fb0e85aebf42b51ba7c4
asyaflesh <asyaflesh@gmail.com>
2023-08-18 09:54:03 +0500

    Added integration tests

--- templates/tests/IntegrationTests/ClientTests/TemplateClientTests.cs
+++ templates/tests/IntegrationTests/ClientTests/TemplateClientTests.cs
@@ -1,14 +1,19 @@
 ﻿namespace Byndyusoft.Template.IntegrationTests.ClientTests
 {
+    using System.Reflection;
     using System.Threading.Tasks;
     using Api;
     using Api.Client.Clients;
     using Api.Client.Settings;
+    using Api.Contracts.TemplateEntity;
+    using DotNet.Testing.Infrastructure.ReadmeGeneration.Entities;
+    using DotNet.Testing.Infrastructure.ReadmeGeneration.Services;
     using Fixtures;
     using FluentAssertions;
     using Microsoft.AspNetCore.Mvc.Testing;
     using Microsoft.Extensions.Options;
     using OpenTracing.Mock;
+    using TestCases;
     using Xunit;
 
     public class TemplateClientTests : IClassFixture<TestServiceProvider>, IClassFixture<WebApplicationFactory<Program>>
@@ -26,14 +31,44 @@
         public async Task GetId_AnyValue_ReturnsSameValue()
         {
             //Arrange
-            var templateId = 1;
+            var testCase = new TestCaseItem
+                               {
+                                   TestId = "TemplateClient",
+                                   Description = "TemplateClient",
+                                   //TestCaseFolder = "TemplateClient",
+                                   Parameters = new TestCaseParameters
+                                                    {
+                                                        TemplateId = 1
+                                                    },
+                                   Mocks = new TestCaseMocks(),
+                                   Expected = new TestCaseExpectations
+                                                  {
+                                                      TemplateDto = new TemplateDto
+                                                                        {
+                                                                            Id = 1
+                                                                        }
+                                                  }
+                               };
 
             //Act
-            var templateDto = await _templateClient.GetTemplate(templateId);
+            var templateDto = await _templateClient.GetTemplate(testCase.Parameters.TemplateId);
 
             //Assert
             templateDto.Should().NotBeNull();
-            templateDto.Id.Should().Be(templateId);
+            templateDto.Id.Should().Be(testCase.Expected.TemplateDto.Id);
+        }
+        
+        [Fact]
+        public async Task AddReport_FromCurrentDomain_ShouldAddReadmeInSolutionRoot()
+        {
+            // ARRANGE
+            var reporter = TestCaseReadmeSolutionReporter.New();
+
+            // ACT
+            var reportConsistency = await reporter.AddReport(Assembly.GetExecutingAssembly());
+
+            // ASSERT
+            reportConsistency.Should().Be(ReportConsistency.Consistent);
         }
     }
 }
