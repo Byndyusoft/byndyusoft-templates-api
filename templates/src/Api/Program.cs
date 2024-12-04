925225f068d44c72c12416110c99b70b6f8df6dc
Nikolay Korsakov <korsakov@byndyusoft.com>
2024-12-04 15:52:03 +0300

    Startup removed Switched to top-level statement

--- templates/src/Api/Program.cs
+++ templates/src/Api/Program.cs
@@ -1,42 +1,69 @@
-namespace Byndyusoft.Template.Api
+using Byndyusoft.Logging.Configuration;
+using Byndyusoft.MaskedSerialization.Serilog.Extensions;
+using Byndyusoft.Template.Api.Infrastructure.OpenTelemetry;
+using Byndyusoft.Template.Api.Infrastructure.Swagger;
+using Byndyusoft.Template.Api.Infrastructure.Versioning;
+using Byndyusoft.Template.Api.Installers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Npgsql;
+using Serilog;
+using System.Text.Json.Serialization;
+
+var serviceName = typeof(Program).Assembly.GetName().Name;
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((context, configuration) =>
+                            configuration
+                                .UseDefaultSettings(context.Configuration)
+                                .UseOpenTelemetryTraces()
+                                .WriteToOpenTelemetry()
+                                .WithMaskingPolicy());
+// Add services to the container.
+var services = builder.Services;
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+
+services.AddOpenTelemetry(serviceName,
+                          builder.Configuration.GetSection("OtlpExporterOptions").Bind,
+                          builder => builder.AddNpgsql(),
+                          builder => builder.AddTemplateMetrics());
+services
+    .AddMvcCore()
+    .AddTracing();
+
+services
+    .AddRouting(options => options.LowercaseUrls = true)
+    .AddControllers()
+    .AddJsonOptions(options => 
+                        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
+services.AddHealthChecks();
+services
+    .AddVersioning()
+    .AddSwagger();
+services
+    .AddRelationalDb(NpgsqlFactory.Instance, builder.Configuration.GetConnectionString("Main"))
+    .AddApplicationServices();
+
+var app = builder.Build();
+app
+    .UseHealthChecks("/healthz")
+    .UseOpenTelemetryPrometheusScrapingEndpoint()
+    .UseRouting()
+    .UseEndpoints(endpoints => endpoints.MapControllers());
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    using Byndyusoft.MaskedSerialization.Serilog.Extensions;
-    using Infrastructure.OpenTelemetry;
-    using Logging.Configuration;
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.Hosting;
-    using Npgsql;
-    using Serilog;
-
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            var serviceName = typeof(Program).Assembly.GetName().Name;
-
-            return Host.CreateDefaultBuilder(args)
-                       .ConfigureServices((context, services) =>
-                       {
-                           services.AddOpenTelemetry(serviceName,
-                                                     context.Configuration.GetSection("OtlpExporterOptions").Bind,
-                                                     builder => builder.AddNpgsql(),
-                                                     builder => builder.AddTemplateMetrics());
-                       })
-                       .ConfigureWebHostDefaults(webBuilder =>
-                       {
-                           webBuilder.UseStartup<Startup>();
-                       })
-                       .UseSerilog((context, configuration) => configuration
-                                                                 .UseDefaultSettings(context.Configuration)
-                                                                 .UseOpenTelemetryTraces()
-                                                                 .WriteToOpenTelemetry()
-                                                                 .WithMaskingPolicy());
-        }
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAuthorization();
+app.Run();
+
+// For tests accessibility
+public partial class Program { }
